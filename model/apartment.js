2691cbc370116638ae2fee4caf45e698cc6a7d57
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-03-24 17:00:49 +0100

    data entyry for small three-room apt

--- model/apartment.js
+++ model/apartment.js
@@ -28,11 +28,29 @@ var apartmentSchema = new Schema({
   area_m2: Number,
   room_count: Number,
   smokedetectors: [{ idnr: String, expires: Date }],
-  watermeters: [{ idnr: String, expires: Date }],
+  coldwatermeters: [{ idnr: String, expires: Date }],
+  warmwatermeters: [{ idnr: String, expires: Date }],
   heatcostallocators: [{ idnr: String, expires: Date, factor: Number }],
   management_cost_eur: Number,
   heating_electrity_cost_eur: Number,
-  landtax_eur: Number,
+  landtax_eur: Number
 });
 
 mongoose.model( 'apartment', apartmentSchema );
+
+sample_apartment_1_drei_zimmer_klein = {
+  "apartment_id": "001-05-03",
+  "owner_id": ObjectId,
+  "area_m2": 86.49,
+  "room_count": 3,
+  "smokedetectors": [{ "idnr": "001-05-03-FL", "expires": 2020-12-31 }, { "idnr": "001-05-03-SZ", "expires": 2020-12-31 }, { "idnr": "001-05-03-KI", "expires": 2020-12-31 }],
+  "coldwatermeters": [{ "idnr": "4169100-BA", "expires": 2020-12-31 }, { "idnr": "524401116-KU", "expires": 2020-12-31 }],
+  "warmwatermeters": [{ "idnr": "4133127-BA", "expires": 2020-12-31 }, { "idnr": "525625030-KU", "expires": 2020-12-31 }],
+  "heatcostallocators": [{ "idnr": "44333607-WO", "expires": 2020-12-31 }, { "idnr": "44333614-BA", "expires": 2020-12-31 }, { "idnr": "44333621-SZ", "expires": 2020-12-31 }, { "idnr": "44333638-KI", "expires": 2020-12-31 }, { "idnr": "44333843-KU", "expires": 2020-12-31 }],
+  "management_cost_eur": 0,
+  "heating_electrity_cost_eur": 0,
+  "landtax_eur": 0
+};
+
+four_sample_apartments = [
+  ]
