feeb4be1891ffe765d0d61e46d40852ba7867aae
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-03-31 16:08:55 +0200

    added person

---
+++ model/person.js
@@ -0,0 +1,30 @@
+// person.js
+//
+// mongo data model definition for a person, human or legal
+//
+// Copyright 2020 by Jeremy Tammik.
+
+var mongoose = require( 'mongoose' );
+
+var Schema = mongoose.Schema;
+
+const enum_person_role = [
+  'occupant',
+  'owner' ];
+
+var personSchema = new Schema({
+  person_id: String,
+  firstname: String,
+  lastname: String,
+  email: String,
+  iban: String,
+  telephone: String,
+  anrede: String,
+  street: String,
+  streetnr: String,
+  zip: String,
+  city: String,
+  country: String
+});
+
+mongoose.model( 'person', personSchema );
