6c980813d503489d0d8a02778cc54b32bf7d601e
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-04-13 14:42:53 +0200

    tried to implement new test, not yet complete

--- loaddata.test.js
+++ loaddata.test.js
@@ -7,3 +7,11 @@ test('loaded five apartments', () => {
 test('loaded nine persons', () => {
   expect(Object.keys(loaddata.persons).length).toBe(9);
 });
+
+//test('apartment has valid owner', () => {
+//  for (var entry of loaddata.apartments) {
+//    var key = entry[0], value = entry[1];
+//    expect(key).toBe(value.apartment_id);
+//    //expect value.owner_id to be a valid key in the list of persons
+//  }  
+//});
