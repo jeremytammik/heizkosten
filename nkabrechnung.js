6e47fabef18ca53ac145dec24dc802f752f959aa
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-04-13 14:57:42 +0200

    started implementing nkabrechnung

---
+++ nkabrechnung.js
@@ -0,0 +1,11 @@
+// nkabrechnung.js
+//
+// algorithm implementing nebenkostenabrechnung for each contract
+//
+// Copyright 2020 by Jeremy Tammik.
+
+/*
+ input: unit, contract, energy_cost_for_this_contract_in_eur
+ output: einzelabrechnung
+*/
+
