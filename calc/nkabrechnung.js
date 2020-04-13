16ac486b8bfb0563a26bbb10ed4452c107dc8a5c
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-04-13 23:32:31 +0200

    moved calculations into calc folder and implemented contract duration calculation

--- calc/nkabrechnung.js
+++ calc/nkabrechnung.js
@@ -18,24 +18,46 @@ const util = require('./util');
 function Nkabrechnung(
   unit,
   contract,
+  year,
   energy_cost_eur )
 {
   // always initialize all instance properties
   this.unit = unit;
   this.contract = contract;
+  this.year = year;
   this.energy_cost_eur = energy_cost_eur;
+
+  // Determine contract duration in given year span
   
+  var begin = Date(year-1, 11, 31);
+  var end =  Date(year, 11, 31);
+  var days_in_year = util.date_diff_days( begin, end ); // 365 or 366!
+  
+  if((contract.end < begin) || (contract.begin > end))
+  {
+    days = 0;
+  }
+  else {
+    if(begin < contract.begin) {
+      begin = contract.begin;
+    }
+    if(contract.end < end) {
+      end = contract.end;
+    }
+    days = util.date_diff_days( begin, end );
+  }
+  var contract_duration = days_in_year / days;
+  
+  // if begin or end are not whole year, use util.date_diff_days
+
   var apartment = loaddata.apartments[contract.apartment];
   
-  var contract_duration = 1.0; // whole year
-
-  // if begin or end are not whole year, use util.date_diff_days
   
   this.vorauszahlungen = 
   this.rueckbehalt = 
   this.hauskosten_umlagefaehig = 
   this.energiekosten = 
-  this.grundsteuer = 
+  this.grundsteuer = apartment.landtax_eur * contract_duration;
   this.rauchmelderwartung = 
 };
 
--- calc/util.test.js
+++ calc/util.test.js
@@ -6,3 +6,10 @@ test('date difference', () => {
   var d = util.date_diff_days( begin, end );
   expect(d).toBe(242);
 });
+
+test('number of days in year', () => {
+  var begin = new Date(2018, 11, 31);
+  var end = new Date(2019, 11, 31);
+  var d = util.date_diff_days( begin, end );
+  expect(d).toBe(365);
+});
