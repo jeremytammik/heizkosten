6833e5cb60b6e08e93311cda133a14c77f1d4865
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-04-13 15:08:05 +0200

    added nebenkostenabrechnung fileds

--- nkabrechnung.js
+++ nkabrechnung.js
@@ -9,3 +9,21 @@
  output: einzelabrechnung
 */
 
+function nkabrechnung(
+  unit,
+  contract,
+  energy_cost_for_this_contract_in_eur )
+{
+  var einzelabrechnung;
+  
+  vorauszahlungen
+  rueckbehalt
+  hauskosten_umlagefaehig
+  energiekosten
+  grundsteuer
+  rauchmelderwartung
+  
+  return einzelabrechnung;
+}
+
+module.exports = nkabrechnung;
