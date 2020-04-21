487c824f69eeeb685e5ceeccaffc7e6a50c94846
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-04-21 16:53:23 +0200

    implemented test for valid unit id and manager

--- data/loaddata.test.js
+++ data/loaddata.test.js
@@ -6,8 +6,8 @@ test('loaded five apartments', () => {
   expect(Object.keys(loaddata.apartments).length).toBe(5);
 });
 
-test('loaded nine persons', () => {
-  expect(Object.keys(loaddata.persons).length).toBe(9);
+test('loaded 10 persons', () => {
+  expect(Object.keys(loaddata.persons).length).toBe(10);
 });
 
 test('apartment has valid owner', () => {
@@ -15,5 +15,13 @@ test('apartment has valid owner', () => {
   for (const [key, value] of Object.entries(loaddata.apartments)) {
     expect(key).toBe( value.apartment_id );
     expect(owners).toContain( value.owner_id );
+  }  
+});
+
+test('unit has valid manager', () => {
+  var owners = Object.keys(loaddata.persons);
+  for (const [key, value] of Object.entries(loaddata.units)) {
+    expect(key).toBe( value.unit_id );
+    expect(owners).toContain( value.manager );
   }  
 });
