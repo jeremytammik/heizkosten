266f528232c5378570ba536decfabe3e93902cce
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-12 15:42:47 +0200

    implemented nkabrechnung test

--- calc/nkabrechnung.test.js
+++ calc/nkabrechnung.test.js
@@ -1,10 +1,9 @@
 const loaddata = require('./loaddata');
 const nkabrechnung = require('./nkabrechnung');
 
-test('nkabreaachnung', () => {
-  for (const [key, value] of Object.entries(loaddata.units)) {
-    for (const [key, value] of Object.entries(loaddata.contracts)) {
-    }
-  }  
+test('test nkabreaachnung', () => {
+  var contract = loaddata.contracts["001-01-04-2018"];
+  var apartment = loaddata.apartments[value.apartment];
+  var unit = loaddata.units[apartment.unit_id];
+  var a = new Nkabrechnung( unit, value, 2018, 0 );
 });
-
