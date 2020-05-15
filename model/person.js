9c75ecdc1d2634dce954b80bb91619216edf1d92
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-15 17:51:07 +0200

    removed unneeded cb argument

--- model/person.js
+++ model/person.js
@@ -34,7 +34,73 @@ function display_string_for_person_doc( p )
     + p.street + ' ' + p.streetnr + ' ' + p.zip + ' ' + p.city + ' ' + p.country;
 }
 
-personSchema.methods.get_display_string = function(cb) {
+personSchema.methods.get_display_string = function() {
+  return display_string_for_person_doc( this );
+};
+
+function generate_person_edit_form_html( p, create_duplicate )
+{
+  var id = p['_id'];
+  delete p['_id'];
+  delete p['__v'];
+  
+  if(create_duplicate) {
+    p['person_id'] = '';
+  }
+  else {
+    delete p['person_id'];
+    delete p['units'];
+  }
+  
+  var action = create_duplicate ? 'dupl' : 'edit';
+
+  var s1 = `\
+<head>\
+	<meta charset="utf-8" />\
+	<title>Edit Person Data</title>\
+  <style>\
+    body, td, label { font-family: sans-serif; font-size: small }\
+    td { text-align: right }\
+    table { border: 1px solid black }\
+  </style>\
+</head>\
+\
+<body>\
+  <p>Person editieren:</p>\
+  <form action="/person/${id}/${action}_submit" method="POST">\
+    <table>\
+`;
+
+var a = [];
+Object.keys(p).forEach( (key,index) => {
+  var k = key;
+  var v = p[key];
+  a.push( `\ 
+<tr>\
+<td><label for="${k}">${k}:</label></td>\
+<td><input type="string" maxlength="30" size="30" placeholder="${k}" id="${k}" name="${k}" value="${v}"></td>\
+</tr>\
+` );
+});
+
+var s2 = a.join('\n');
+
+var s3 = '\
+        <tr>\
+          <td colspan="2" style="text-align: center">\
+            <button type="submit">Speichern</button>\
+          </td>\
+        </tr>\
+      </table>\
+    </form>\
+  </body>\
+</html>\
+';
+
+return s1 + s2 + s3;
+}
+
+personSchema.methods.get_edit_form_html = function(cb) {
   return display_string_for_person_doc( this );
 };
 
