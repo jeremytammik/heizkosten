f4cbfd63fd343ff77078140cc86c43ae00e01784
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-16 19:21:21 +0200

    cleaned up cost schema

--- app/model/cost.js
+++ app/model/cost.js
@@ -12,43 +12,56 @@ var costSchema = new Schema({
   unit_id: mongoose.ObjectId,
   year: Number,
   
-  // hausgeld_umlagefaehig_eur -- zahlen mieter
+  // hausgeld nicht umlagefaehig, zahlen eigentuemer
   
-  allocatable: {
-    kabelgebuehren: Number,
-    allgemeinstrom: Number,
-    muellgebuehren_hausmeister: Number,
-    streu_und_putzmittel: Number,
-    aussenanlage_pflege: Number,
-    versicherungen: Number,
-    niederschlagswasser: Number,
-    trinkwasseruntersuchung: Number,
-    material_und_hilfsstoffe: Number,
-    reinigung: Number,
-    hausmeister_sozialabgaben: Number,
-    hausservice_fremdfirmen: Number,
-    lift_umlagefaehig: Number,
-    feuerloescher_wartung: Number,
-    wartung_eingangstueren: Number,
-    wartung_lueftungsanlage: Number
-  },
+  hausgeld_eur: Number, 
 
-  hausgeld_eur: Number, // zahlen eigentuemer
+  // hausgeld umlagefaehig, zahlen mieter
+  
+  kabelgebuehren: Number, // pro wohnung
+  
+  // pro quadratmeter mit nebenkosten_anteil_schluessel
+  
+  allgemeinstrom: Number,
+  muellgebuehren_hausmeister: Number,
+  streu_und_putzmittel: Number,
+  aussenanlage_pflege: Number,
+  versicherungen: Number,
+  niederschlagswasser: Number,
+  trinkwasseruntersuchung: Number,
+  material_und_hilfsstoffe: Number,
+  reinigung: Number,
+  hausmeister_sozialabgaben: Number,
+  hausservice_fremdfirmen: Number,
+  lift_umlagefaehig: Number,
+  feuerloescher_wartung: Number,
+  wartung_eingangstueren: Number,
+  wartung_lueftungsanlage: Number,
 
   // gesamtkosten heizung kaltwasser warmwasser HKW (5 items):
+  
   // 1. brennstoff - fuel
+
   fuel_consumption_kwh: Number,
   fuel_consumption_eur: Number,
+
   // 2. heiznebenkosten
+
   heating_nk_consumption_allocation_eur: Number,
   heating_nk_maintenance_eur: Number,
   heating_nk_electrity_eur: Number,
   heating_nk_chimneysweep_eur: Number,
+
   // 3. heizzusatzkosten - kosten heizgeraete
+
   heating_zk_heizgeraete_eur: Number,
+  
   // 4. zusatzkosten warmwasser
+
   hotwater_zk_eur: Number,
+
   // 5. hausnebenkosten (kaltwasseraufbereitung und entsorgung) enthaelt u.a. (5 positionen):
+
   house_nk_coldwater_m3: Number,
   house_nk_coldwater_eur: Number,
   house_nk_coldwater_equipment_eur: Number,
