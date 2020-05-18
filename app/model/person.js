ec7613b28b8249cd03a491f21f07bd5254da30e4
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-18 22:06:55 +0200

    implemented more validators for person db entry

--- app/model/person.js
+++ app/model/person.js
@@ -5,7 +5,6 @@
 // Copyright 2020 by Jeremy Tammik.
 
 var mongoose = require( 'mongoose' );
-const { non_empty_alpha_mumeric, empty_or_ascii_or_umlaut } = require( '../../data/jtregex' );
 
 var Schema = mongoose.Schema;
 
@@ -13,13 +12,13 @@ var Schema = mongoose.Schema;
 //  'occupant',
 //  'owner' ];
 
+//const { non_empty_alpha_mumeric, empty_or_ascii_or_umlaut } = require( '../../data/jtregex' );
 const regex_valid_name_chars = /^[äöü\w][äöü\w\ ]*$/;
 const regex_valid_email_address = /^[äöü\w_.+\-]+@[äöü\w\-]+\.[äöü\w\-\.]+$/;
 const regex_valid_iban = /^([a-zA-Z]{2})(\d{2})([a-zA-Z\d ]+)$/;
 const regex_valid_telephone_number = /^[0-9\+][0-9\/\- ]{4,20}$/;
-const regex_empty_or_alnum_or_umlaut_with_space = /^[0-9a-zA-Zäöü\-\ ]*$/;
-const regex_empty_or_alnum_or_umlaut_with_space = /^[0-9a-zA-Zäöü\-\ ]*$/;
-const regex_empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
+//const regex_empty_or_alnum_or_umlaut_with_space = /^[0-9a-zA-Zäöü\-\ ]*$/;
+//const regex_empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
 
 var personSchema = new Schema({
   _id: {  // suppress automatic generation  
