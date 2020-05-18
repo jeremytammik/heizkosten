8b130ccc3d40ab5ad536a178db8e0683ac7b1f1f
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-18 22:09:46 +0200

    store regex in jtregex.js

--- app/model/person.js
+++ app/model/person.js
@@ -12,13 +12,12 @@ var Schema = mongoose.Schema;
 //  'occupant',
 //  'owner' ];
 
-//const { non_empty_alpha_mumeric, empty_or_ascii_or_umlaut } = require( '../../data/jtregex' );
-const regex_valid_name_chars = /^[äöü\w][äöü\w\ ]*$/;
-const regex_valid_email_address = /^[äöü\w_.+\-]+@[äöü\w\-]+\.[äöü\w\-\.]+$/;
-const regex_valid_iban = /^([a-zA-Z]{2})(\d{2})([a-zA-Z\d ]+)$/;
-const regex_valid_telephone_number = /^[0-9\+][0-9\/\- ]{4,20}$/;
-//const regex_empty_or_alnum_or_umlaut_with_space = /^[0-9a-zA-Zäöü\-\ ]*$/;
-//const regex_empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
+const {
+  regex_valid_name_chars,
+  regex_valid_email_address,
+  regex_valid_iban,
+  regex_valid_telephone_number
+} = require( '../../data/jtregex' );
 
 var personSchema = new Schema({
   _id: {  // suppress automatic generation  
--- data/jtregex.js
+++ data/jtregex.js
@@ -5,8 +5,22 @@ const date_format = /^\d{4}-\d{2}-\d{2}$/;
 const non_empty_alpha_mumeric = /^[0-9a-zA-Z\_]+$/;
 const empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
 
+// used in model/person
+
+const regex_valid_name_chars = /^[äöü\w][äöü\w\ ]*$/;
+const regex_valid_email_address = /^[äöü\w_.+\-]+@[äöü\w\-]+\.[äöü\w\-\.]+$/;
+const regex_valid_iban = /^([a-zA-Z]{2})(\d{2})([a-zA-Z\d ]+)$/;
+const regex_valid_telephone_number = /^[0-9\+][0-9\/\- ]{4,20}$/;
+
+//const regex_empty_or_alnum_or_umlaut_with_space = /^[0-9a-zA-Zäöü\-\ ]*$/;
+//const regex_empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
+
 module.exports = {
   date_format,
   non_empty_alpha_mumeric,
-  empty_or_ascii_or_umlaut
+  empty_or_ascii_or_umlaut,
+  regex_valid_name_chars,
+  regex_valid_email_address,
+  regex_valid_iban,
+  regex_valid_telephone_number
 };
