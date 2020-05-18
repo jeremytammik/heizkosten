2ae609ead4691cde17479dc1ed758306be7ef022
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-18 14:07:46 +0200

    wrong parenthesis

--- app/calc/util.test.js
+++ app/calc/util.test.js
@@ -12,7 +12,7 @@ test('trimAllFieldsInObjectAndChildren', () => {
   //assert.true(_.isEqual(util.trimAllFieldsInObjectAndChildren({'a ': ' bob ', 'b': [{'c ': {'d': 'e '}}, {' f ': ' g ' }]}), {'a': 'bob', 'b': [{'c': {'d': 'e'}}, {'f': 'g' }]}));
   //assert.true(_.isEqual(util.trimAllFieldsInObjectAndChildren(' bob '), 'bob'));
   
-  expect(util.trimAllFieldsInObjectAndChildren('2 ').toBe('2'));
+  expect(util.trimAllFieldsInObjectAndChildren('2 ')).toBe('2');
   expect(util.trimAllFieldsInObjectAndChildren(['2 ', ' bob '])).toBe(['2', 'bob']);
   expect(util.trimAllFieldsInObjectAndChildren({'b ': ' bob '})).toBe({'b': 'bob'});
   expect(util.trimAllFieldsInObjectAndChildren({'b ': ' bob ', 'c': 5, d: true })).toBe({'b': 'bob', 'c': 5, d: true});
