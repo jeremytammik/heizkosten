dbc9aaa2191c4475694d7db33f4b450ae7d4190c
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-19 14:42:30 +0200

    implemented jtformgen_list_documents

--- app/routes/rperson.js
+++ app/routes/rperson.js
@@ -13,8 +13,7 @@ app.get('/api/v1/person/unit/:uid', PersonService.findAllForUnit);
 
 const {
   success_with_document_count,
-  jtformgen_confirm_delete,
-  jtformgen_list_documents } = require('../form/jtformgen.js');
+  jtformgen_confirm_delete } = require('../form/jtformgen.js');
 
 app.get( '/unit/:uid/list', (req, res) => {
   var uid = req.params.uid;
@@ -46,9 +45,9 @@ app.get( '/:id/edit', (req, res) => {
   Person.find( {'_id': id }, (err, results) => {
     if (err) { return console.log(err); }
     else {
-      //var doc = results[0]._doc;
-      //var form = Person.get_edit_form_html( doc, false );
-      //res.send( form );
+      var doc = results[0]._doc;
+      var form = Person.get_edit_form_html( doc, false );
+      res.send( form );
     }
   });
 });
@@ -67,8 +66,8 @@ app.post( '/:id/edit_submit', (req, res) => {
   Person.updateOne( { "_id": id }, p, (err,res2) => {
     if (err) { return console.error(err); }
     Person.countDocuments( {}, (err, count) => {
-      //if (err) { return console.error(err); }
-      //return res.send( success_with_document_count( count.toString(), 'person' ) );
+      if (err) { return console.error(err); }
+      return res.send( success_with_document_count( count.toString(), 'person' ) );
     });
   });
 });
@@ -78,9 +77,9 @@ app.get( '/:id/dupl', (req, res) => {
   Person.find( {'_id': id }, (err, results) => {
     if (err) { return console.log(err); }
     else {
-      //var doc = results[0]._doc;
-      //var form = Person.get_edit_form_html( doc, true );
-      //res.send( form );
+      var doc = results[0]._doc;
+      var form = Person.get_edit_form_html( doc, true );
+      res.send( form );
     }
   });
 });
@@ -92,30 +91,30 @@ app.post( '/:id/dupl_submit', (req, res) => {
   //console.log('id_original', id_original, 'id', id, 'p0', p);
   Person.countDocuments( {'_id': id }, (err, count) => {
     if (err) {
-      //return console.error(err);
+      return console.error(err);
     }
     //console.log('count', count);
     if( 0 < count ) {
-      //var error = { 'errors': { '_id': { 'path': '_id', 'message': 'duplicate id' }}};
-      //var form = Person.get_edit_form_html( req.body, true, error );
-      //return res.send( form );
+      var error = { 'errors': { '_id': { 'path': '_id', 'message': 'duplicate id' }}};
+      var form = Person.get_edit_form_html( req.body, true, error );
+      return res.send( form );
     }
     //console.log('p1', p);
     var person = new Person( p );
     error = person.validateSync();
     if( error ) {
-      //var form = Person.get_edit_form_html( p, true, error );
-      //return res.send( form );      
+      var form = Person.get_edit_form_html( p, true, error );
+      return res.send( form );      
     }
     //console.log('p2', p);
     Person.create( req.body, (err2,res2) => {
-      //if (err2) {
-      //  return console.error(err2);
-      //}
-      //Person.countDocuments( {}, (err3, count) => {
-      //  if (err3) { return console.error(err3); }
-      //  return res.send( success_with_document_count( count.toString(), 'person' ) );
-      //});
+      if (err2) {
+        return console.error(err2);
+      }
+      Person.countDocuments( {}, (err3, count) => {
+        if (err3) { return console.error(err3); }
+        return res.send( success_with_document_count( count.toString(), 'person' ) );
+      });
     });
   });
 });
@@ -125,8 +124,8 @@ app.get( '/:id/del', (req, res) => {
   Person.find( {'_id': id }, (err, results) => {
     if (err) { return console.log(err); }
     else {
-      //var s = results[0].get_display_string();
-      //res.send( jtformgen_confirm_delete( s, id ) );
+      var s = results[0].get_display_string();
+      res.send( jtformgen_confirm_delete( s, id ) );
     }
   });
 });
@@ -136,8 +135,8 @@ app.get( '/:id/del_confirmed', (req, res) => {
   Person.deleteOne( {'_id': id }, (err, results) => {
     if (err) { return console.log(err); }
     Person.countDocuments( {}, (err, count) => {
-      //if (err) { return console.error(err); }
-      //return res.send( success_with_document_count( count.toString(), 'person' ) );
+      if (err) { return console.error(err); }
+      return res.send( success_with_document_count( count.toString(), 'person' ) );
     });
   });
 });
@@ -146,16 +145,16 @@ app.get( '/load_sample_data', (req, res) => {
   var fs = require('fs');
   var persons = JSON.parse(
     fs.readFileSync(
-      //'data/person.json', 'utf8' ));
+      'data/person.json', 'utf8' ));
   
   Person.deleteMany( {}, (err) => {
     if (err) { return console.error(err); }
     Person.create( Object.values(persons), (err,res2) => {
-      //if (err) { return console.error(err); }
-      //Person.countDocuments( {}, (err, count) => {
-      //  if (err) { return console.error(err); }
-      //  return res.send( success_with_document_count( count.toString(), 'person' ) );
-      //});
+      if (err) { return console.error(err); }
+      Person.countDocuments( {}, (err, count) => {
+        if (err) { return console.error(err); }
+        return res.send( success_with_document_count( count.toString(), 'person' ) );
+      });
     });
   });
 });
@@ -165,18 +164,18 @@ app.get( '/save_sample_data', (req, res) => {
     if (err) { return console.error(err); }
     var persons = {};
     docs.forEach( (doc) => {
-      //var p = doc._doc;
-      //delete p['__v'];
-      //persons[p._id] = p;
+      var p = doc._doc;
+      delete p['__v'];
+      persons[p._id] = p;
     });
     var fs = require('fs');
     var fn = 'data/tmp/person.json';
     fs.writeFile( fn,
-      //JSON.stringify( persons, null, 2 ), 'utf8',
-      //function (err) {
-      //  if (err) { return console.log(err); }
-      //  return res.send( `Person data saved in '${fn}'` );
-      //}
+      JSON.stringify( persons, null, 2 ), 'utf8',
+      function (err) {
+        if (err) { return console.log(err); }
+        return res.send( `Person data saved in '${fn}'` );
+      }
     );
   });
 });
@@ -193,14 +192,14 @@ app.post( '/create_new_submit', (req, res) => {
     var person = new Person( p );
     error = person.validateSync();
     if( error ) {
-      //var form = Person.get_edit_form_html( p, false, error );
-      //return res.send( form );      
+      var form = Person.get_edit_form_html( p, false, error );
+      return res.send( form );      
     }
     Person
-      //.create( p )
-      //.then( person =>
-      //  Person.countDocuments( {}, (err, count) => {
-      //    if (err) { return console.error(err); }
-      //    return res.send( success_with_document_count( count.toString(), 'person' ) );
-      //  }));
+      .create( p )
+      .then( person =>
+        Person.countDocuments( {}, (err, count) => {
+          if (err) { return console.error(err); }
+          return res.send( success_with_document_count( count.toString(), 'person' ) );
+        }));
 });
