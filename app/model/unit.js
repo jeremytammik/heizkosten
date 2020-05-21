395f8ad45064e94923ccb0e09c4aecbca5640f80
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-21 17:16:52 +0200

    removed comments

--- app/model/unit.js
+++ app/model/unit.js
@@ -113,29 +113,6 @@ var unitSchema = new Schema({
   
   costs: [mongoose.ObjectId],
 
-  //hausgeld_eur: Number, // zahlen eigentuemer
-  //hausgeld_umlagefaehig_eur: Number, // zahlen mieter
-
-  //// gesamtkosten heizung kaltwasser warmwasser HKW (5 items):
-  //// 1. brennstoff - fuel
-  //fuel_consumption_kwh: Number,
-  //fuel_consumption_eur: Number,
-  //// 2. heiznebenkosten
-  //heating_nk_consumption_allocation_eur: Number,
-  //heating_nk_maintenance_eur: Number,
-  //heating_nk_electrity_eur: Number,
-  //heating_nk_chimneysweep_eur: Number,
-  //// 3. heizzusatzkosten - kosten heizgeraete
-  //heating_zk_heizgeraete_eur: Number,
-  //// 4. zusatzkosten warmwasser
-  //hotwater_zk_eur: Number,
-  //// 5. hausnebenkosten (kaltwasseraufbereitung und entsorgung) enthaelt u.a. (5 positionen):
-  //house_nk_coldwater_m3: Number,
-  //house_nk_coldwater_eur: Number,
-  //house_nk_coldwater_equipment_eur: Number,
-  //house_nk_coldwater_allocation_fee_eur: Number,
-  //house_nk_coldwater_sonderkoster_eur: Number,
-  
   payments: [{
     date: Date,
     amount: Number,
