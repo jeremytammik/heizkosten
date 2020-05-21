fdf7afb6c5f1b9aafbd03dbd346edc1d1ee9be3a
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-21 11:35:05 +0200

    errors in jtformgen_confirm_delete

--- app/form/jtformgen.js
+++ app/form/jtformgen.js
@@ -33,7 +33,7 @@ function success_with_document_count( n, thing_en )
 function jtformgen_confirm_delete( route, thing_de, description, id )
 {
   return wrap_html( '<body>'
-    + `<p>Sollen die Daten der folgenden ${thing_display} wirklich geloescht werden?</p>`
+    + `<p>Sollen die Daten der folgenden ${thing_de} wirklich geloescht werden?</p>`
     + `<ul><li>${description}</li></ul>`
     + `<button><a href="/${thing}/${id}/del_confirmed">Ja</a></button> &ndash; `
     + '<button><a href="/hauskosten.html">Nein</a></button>' );
