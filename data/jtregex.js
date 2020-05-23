e4e06c89f581fc4b607a643c17cd753a7268b5ec
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-23 15:44:39 +0200

    implemented date_with_optional_time_format

--- data/jtregex.js
+++ data/jtregex.js
@@ -1,6 +1,6 @@
 // https://mariusschulz.com/blog/deserializing-json-strings-as-javascript-date-objects
 //const date_time_format = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
-const date_format = /^\d{4}-\d{2}-\d{2}$/;
+const date_with_optional_time_format = /^\d{4}-\d{2}-\d{2}[T\d\:Z]{,14}$/;
 
 const non_empty_alpha_mumeric = /^[0-9a-zA-Z\_]+$/;
 const empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \:\;\.\,\-\+\_\@]*$/;
@@ -18,7 +18,7 @@ const regex_valid_telephone_number = /^[0-9\+][0-9\/\- ]{4,20}$/;
 //const regex_empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
 
 module.exports = {
-  date_format,
+  date_with_optional_time_format,
   non_empty_alpha_mumeric,
   empty_or_ascii_or_umlaut,
   
