9b508a5c029e5d32e86887e7d80b368215386d05
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-26 11:35:09 +0200

    extract ids from results

--- app/routes/rperson.js
+++ app/routes/rperson.js
@@ -80,10 +80,12 @@ app.post( '/unit/:uid/list', (req, res) => {
   };`
   o.reduce = 'function (k, vals) { return Array.sum(vals); };'
   o.query = { units : "001"};
+  o.out = { replace: 'match_results' };
   Person.mapReduce( o, function (err, results) {
     if (err) { return console.log(err); }
     else {
-      console.log(results);
+      var ids = results.results;
+      console.log(ids);
       var url_filter = `/person/unit/${uid}/list`;
       var matching = sfilter
         ? ` matching "${sfilter}"`
