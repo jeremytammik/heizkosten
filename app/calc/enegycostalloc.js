b03f36b491e29fffa8667d4ac5f76e04cec102c4
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-26 13:34:24 +0200

    implemented energy cost alloc test

--- app/calc/enegycostalloc.js
+++ app/calc/enegycostalloc.js
@@ -13,31 +13,18 @@
 const loaddata = require('./loaddata');
 const util = require('./util');
 
-function Energycostalloc(
-  contract_id,
-  year ) // todo, possibly: support flexible begin and end date
+// Determine contract duration in given year span
+function get_contract_duration_in_given_year( contract, begin, end )
 {
-  var contract = loaddata.contracts[contract_id];
-  var apartment = loaddata.apartments[contract.apartment_id];
-  var unit = loaddata.units[apartment.unit_id];
-  var costs = loaddata.costs[apartment.unit_id + '-' + year.toString()];
+  // adjust begin and end to contract begin and end in given year
   
-  var cw_numbers = Object.keys(apartment.coldwatermeters);
-  var cw_readings = contract.coldwatermeters;
-  
-  // Determine contract duration in given year span
-
-  var begin = new Date( year-1, 11, 31 );
-  var end =  new Date( year, 11, 31 );
-  
-  console.log('year begin and end: ' + begin.toString() + ', ' + end.toString() );
-  
-  var days_in_year = util.date_diff_days( begin, end ); // 365 or 366!
-  var days = days_in_year;
-  
-  if((contract.end < begin) || (contract.begin > end))
+  if(contract.end < begin)
   {
-    days = 0;
+    return begin, begin;
+  }
+  else if (contract.begin > end)
+  {
+    return end, end;
   }
   else {
     if(begin < contract.begin) {
@@ -46,13 +33,29 @@ function Energycostalloc(
     if(contract.end < end) {
       end = contract.end;
     }
-    days = util.date_diff_days( begin, end );
   }
+  return begin, end;
+}
+
+function Energycostalloc(
+  contract_id,
+  year ) // todo, possibly: support flexible begin and end date
+{
+  var contract = loaddata.contracts[contract_id];
+  var apartment = loaddata.apartments[contract.apartment_id];
+  var unit = loaddata.units[apartment.unit_id];
+  var costs = loaddata.costs[apartment.unit_id + '-' + year.toString()];
+
+  var begin = new Date( year-1, 11, 31 );
+  var end =  new Date( year, 11, 31 );
+  var begin, end = get_contract_duration_in_given_year( contract, begin, end );
   
-  var contract_duration = days_in_year / days;
+  var cw_numbers = Object.keys(apartment.coldwatermeters);
+  var cw_readings = contract.coldwatermeters;
+  
+  console.log(`contract begin and end in ${year}: ` + begin.toString() + ', ' + end.toString() );
   
   
-//  console.log(
 //    "coldwatermeters": { "KW-BA-4007049": "2020-12-31", "KW-KU-52440355": "2020-12-31"},
 //    "hotwatermeters": { "WW-BA-4133058": "2020-12-31", "WW-KU-604465496": "2020-12-31"},
 //    "heatcostallocators": { "HE-WO-44322335": "2020-12-31, 1.35", "HE-BA-443323257": "2020-12-31, 0.375", "HE-SM-44323240": "2020-12-31, 0.975", "HE-SK-443323233": "2020-12-31, 0.675", "HE-KU-44322328": "2020-12-31, 0.575"},
---
+++ app/calc/enegycostalloc.test.js
@@ -0,0 +1,6 @@
+const Energycostalloc = require('./energycostalloc');
+
+test('test energycostalloc', () => {
+  var contract_id = "001-01-04-2018";
+  var a = new Energycostalloc( contract_id, 2018 );
+});
