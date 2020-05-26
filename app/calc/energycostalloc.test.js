7041b7608f99a1fd9a5618f53262d5dc32074e70
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-26 13:45:38 +0200

    error braeuning should start in february

--- app/calc/energycostalloc.test.js
+++ app/calc/energycostalloc.test.js
@@ -1,8 +1,9 @@
+const loaddata = require('../../data/loaddata');
 const Energycostalloc = require('./energycostalloc');
 
-test('test energycostalloc', () => {
-  var ids = Object.keys( loaddata.contracts );
+test( 'test energycostalloc', () => {
+  var ids = ["001-12-06-2018"]; // Object.keys( loaddata.contracts );
   ids.forEach( (id) => {
     var a = new Energycostalloc( id, 2018 );
-  }
+  });
 });
