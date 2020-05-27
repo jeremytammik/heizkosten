0f020aef69ca4b2ec154267a5472a1dd9a8aab36
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-27 19:51:30 +0200

    added begin and end of line locking to all validator regex

--- data/jtregex.js
+++ data/jtregex.js
@@ -7,17 +7,17 @@ const empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \:\;\.\,\-\+\_\@]*$/;
 
 // used in model/person
 
-const regex_valid_person_id = /[0-9a-z_]{1,20}/; // non-empty lowercase alphanumeric with underscore max 20 length
-const regex_valid_unit_list = /[0-9,]{3,40}/; // comma-separated digits only, min 3 max 40 length
+const regex_valid_person_id = /^[0-9a-z_]{1,20}$/; // non-empty lowercase alphanumeric with underscore max 20 length
+const regex_valid_unit_list = /^[0-9,]{3,40}$/; // comma-separated digits only, min 3 max 40 length
 const regex_valid_name_chars = /^[äöü\w][äöü\w\ ]*$/;
 const regex_valid_email_address = /^[äöü\w_.+\-]+@[äöü\w\-]+\.[äöü\w\-\.]+$/;
 const regex_valid_iban = /^([a-zA-Z]{2})(\d{2})([a-zA-Z\d ]+)$/;
 const regex_valid_telephone_number = /^[0-9\+][0-9\/\- ]{4,20}$/;
-const regex_valid_apartment_id = /[0-9]{3}-[0-9]{2}-[0-9]{2}/;
-const regex_valid_unit_id = /[0-9]{3}/;
-const regex_valid_meter_id = /(?:RA|HE|KW|WW)-(?:KU|BA|FL|SK|SM|SG|WO)-[0-9\-]{1,12}/;
-const regex_valid_date = /(?:19|20)[0-9]{2}-[0-9]{2}-[0-9]{2}/;
-const regex_valid_meter_expiry_with_factor = /(?:19|20)[0-9]{2}-[0-9]{2}-[0-9]{2}\ *\,\ *[0-9\.]{1,7}/;
+const regex_valid_apartment_id = /^[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+const regex_valid_unit_id = /^[0-9]{3}$/;
+const regex_valid_meter_id = /^(?:RA|HE|KW|WW)-(?:KU|BA|FL|SK|SM|SG|WO)-[0-9\-]{1,12}$/;
+const regex_valid_date = /^(?:19|20)[0-9]{2}-[0-9]{2}-[0-9]{2}$/;
+const regex_valid_meter_expiry_with_factor = /^(?:19|20)[0-9]{2}-[0-9]{2}-[0-9]{2}\ *\,\ *[0-9\.]{1,7}$/;
 
 //const regex_empty_or_alnum_or_umlaut_with_space = /^[0-9a-zA-Zäöü\-\ ]*$/;
 //const regex_empty_or_ascii_or_umlaut = /^[0-9a-zA-Zäöü\ \;\.\,\-\+\_\@]*$/;
