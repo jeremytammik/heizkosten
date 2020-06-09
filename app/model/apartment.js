33e55bc603504e19ddd5b1b4bd954fe5ca97fb24
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-06-09 19:31:12 +0200

    removed comment

--- app/model/apartment.js
+++ app/model/apartment.js
@@ -95,9 +95,6 @@ var apartmentSchema = new Schema({
     max: 20,
     validate: {
       validator: function(s) {
-        // todo: add a test that the non-empty person id
-        // really matches an entry in the person database
-        //console.log(s, '-->', regex_valid_person_id.test(s));
         return (!s) || regex_valid_person_id.test(s);
       },
       message: props => `'${props.value}' is not a valid person_id`
