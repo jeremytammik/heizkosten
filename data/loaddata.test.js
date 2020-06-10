37e021d821ce04ca47c173161fdbc092c9f6de73
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-06-10 08:57:19 +0200

    export jtregex object

--- data/loaddata.test.js
+++ data/loaddata.test.js
@@ -63,7 +63,7 @@ test('cost has valid unit and year matching dictionary key', () => {
 });
 
 test('loaded N persons', () => {
-  expect(Object.keys(loaddata.persons).length).toBe(13);
+  expect(Object.keys(loaddata.persons).length).toBe(12);
 });
 
 test('person id matches dictionary key', () => {
