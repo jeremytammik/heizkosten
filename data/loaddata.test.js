ac017e830addc98231a1ea0de61632c94cd56338
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-06-23 14:38:06 +0200

    updated person.json

--- data/loaddata.test.js
+++ data/loaddata.test.js
@@ -52,7 +52,7 @@ test('cost has valid unit and year matching dictionary key', () => {
 });
 
 test('loaded N persons', () => {
-  expect(Object.keys(loaddata.persons).length).toBe(109);
+  expect( Object.keys( loaddata.persons ).length ).toBe( 118 );
 });
 
 test('person id matches dictionary key', () => {
