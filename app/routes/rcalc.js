302bc3d6488b0cfaf54271d764f04d010d6fac2e
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-06-25 16:22:07 +0200

    rewrote /nk/unit/:uid/year/:year route to use findById and Coal object

--- app/routes/rcalc.js
+++ app/routes/rcalc.js
@@ -3,6 +3,7 @@ const jtformgen = require('../form/jtformgen');
 const nkabrechnung = require('../calc/nkabrechnung');
 const Apartment = require( '../model/apartment' );
 const Contract = require( '../model/contract' );
+const Coal = require( '../calc/coal' );
 const Cost = require( '../model/cost' );
 const Person = require( '../model/person' );
 const Unit = require( '../model/unit' );
@@ -20,16 +21,13 @@ app.get( '/nk/unit/:uid/year/:year', (req, res) => {
       'unit_id': uid,
       'begin': {$lte: year_end},
       $or: [ {'end':''}, {'end': {$gte: year_begin}} ]
-    }, (e1, ctrcts) => {
+    }, (e1, contracts) => {
     if ( e1 ) { console.error( e1 ); return res.send( e1.toString() ); }
-    // extract apartment and occupant ids and
-    // reorganise contracts into dictionary
+    // extract apartment and occupant ids
     var n1 = ctrcts.length;
-    var contracts = {};
     var apt_ids = [];
     var p_ids = [];
     for( let i = 0; i < n1; ++i ) {
-      contracts[ctrcts[i]._id] = ctrcts[i]._doc;
       apt_ids.push( ctrcts[i].apartment_id );
       p_ids.push( ctrcts[i].occupant_ids[0] );
     }
@@ -41,10 +39,10 @@ app.get( '/nk/unit/:uid/year/:year', (req, res) => {
       for( let i = 0; i < n2; ++i ) {
         apartments[apts[i]._id] = apts[i];
       }
-      Unit.find( { '_id': uid }, (e3, units) => {
+      Unit.findById( uid, (e3, unit) => {
         if ( e3 ) { console.error( e3 ); return res.send( e3.toString() ); }
         var cost_id = uid + '-' + year;
-        Cost.find( { '_id': cost_id }, (e4, costs) => {
+        Cost.findById( cost_id, (e4, costs) => {
           if ( e4 ) { console.error( e4 ); return res.send( e4.toString() ); }
           Person.find( { '_id': {$in : p_ids} }, (e5, persons) => {
             if ( e5 ) { console.error( e5 ); return res.send( e5.toString() ); }
@@ -54,9 +52,7 @@ app.get( '/nk/unit/:uid/year/:year', (req, res) => {
             for( let i = 0; i < n5; ++i ) {
               addressees[persons[i]._id] = persons[i];
             }
-            // iterate over contracts
-            var keys = Object.keys( contracts );
-            keys.sort();
+
             var title = `Nebenkostenabrechnung ${year} äöü`;
             a = [];
             
@@ -70,16 +66,19 @@ app.get( '/nk/unit/:uid/year/:year', (req, res) => {
             var pdf = new jsPDF();
             pdf.text( title, 10, 10 );
             
+            var ucoals = {}; // map contract id to coal
+            
             for( let i = 0; i < n1; ++i ) {
-              var contract = contracts[keys[i]];
-              var unit = units[0]._doc;
-              var year_costs = costs[0]._doc;
+              var contract = contracts[i];
+              var unit = unit._doc;
+              var year_costs = costs._doc;
               var apartment = apartments[contract.apartment_id]._doc;
               var addressee = addressees[contract.occupant_ids[0]]._doc;
               var energy_cost_eur = 907.54;
-              a.push( nkabrechnung.get_nkabrechnung_for(
-                unit, year_costs, apartment, contract, addressee,
-                year, energy_cost_eur ) );              
+              
+              ucoals[contract._id] = new Coal(
+                unit, year_costs, apartment, contract,
+                addressee, year, energy_cost_eur );
             }
             
             // PDF teardown
@@ -91,7 +90,7 @@ app.get( '/nk/unit/:uid/year/:year', (req, res) => {
             delete global.navigator;
             delete global.btoa;            
             
-            return res.send( jtformgen.nkabrechnung_report_html( year, a, fn ) );
+            return res.send( jtformgen.nkabrechnung_report_html( year, ucoals ) );
           });
         });
       });
