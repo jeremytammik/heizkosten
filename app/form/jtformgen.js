c65d4f14e522c84f496e5c722061d525f7a39cf2
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-07-07 00:16:20 +0200

    shift labels further right

--- app/form/jtformgen.js
+++ app/form/jtformgen.js
@@ -288,7 +288,7 @@ function nkabrechnung_report( uid, year, map_contract_to_coal )
     
     doc.text( 10, 80, lines );
     
-    doc.text( 10, 200, labels, {'align': 'right'} );
+    doc.text( 100, 200, labels, {'align': 'right'} );
   }
   
   // PDF teardown
