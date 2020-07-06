7d2a57d6e51cbd84393164820a6773954fba61ac
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-07-06 14:01:37 +0200

    otto entered energy costs

--- app/model/contract.js
+++ app/model/contract.js
@@ -106,7 +106,7 @@ var contractSchema = new Schema({
   //coldwatermeters: { type: Object }, // dictionary mapping meter number to a list of readings, dictionary mapping date to amount
   //hotwatermeters: { type: Object }, // dictionary mapping meter number to a list of readings, dictionary mapping date to amount
   smokedetector_maintenance_cost_eur: Number, // constant defined by owner per detector replaced
-  energiekosten_2019_eur: Number } // temporary one-off field using third-party input data for 2019 only
+  energiekosten_2019_eur: Number }, // temporary one-off field using third-party input data for 2019 only
   { _id: false } // suppress automatic generation
 );
 
