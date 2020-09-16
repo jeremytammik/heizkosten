518fc0d3bd9a9e82d862524d1160c2b977d3a7e6
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-09-16 22:10:53 +0200

    fix validate_begin_end_date

--- data/jtvalidators.js
+++ data/jtvalidators.js
@@ -1,5 +1,13 @@
 const jtregex = require( './jtregex' );
 
+function validate_begin_end_date( s ) {
+  if( !jtregex.valid_date.test( s ) ) { return false; }
+  //const mon = 1 * str.slice(5, 7);
+  //const mid_month_begin_date = [16,15,16,16,16,...]
+  const day = 1 * s.slice(8, 10);
+  return 1 == day || 15 == day || 16 == day;
+}
+
 const jtvalidators = {
 
 // validate meter data: date [, factor], date: amount [, date: amount]...
@@ -22,13 +30,7 @@ validate_meter_data: function( s, with_factor ) {
   return true;
 },
 
-validate_begin_end_date: function( s ) {
-  if( !jtregex.valid_date.test( s ) ) { return false; }
-  //const mon = 1 * str.slice(5, 7);
-  //const mid_month_begin_date = [16,15,16,16,16,...]
-  const day = 1 * s.slice(8, 10);
-  return 1 == day || 15 == day || 16 == day;
-},
+validate_begin_end_date: validate_begin_end_date,
 
 validate_dict_date_amount_string: function( s ) {
   var a = s.trim().split( ',' );
@@ -36,7 +38,7 @@ validate_dict_date_amount_string: function( s ) {
   for( let i = 0; i < n; ++i ) {
     var b = a[i].split( ':' );
     if( !(2 === b.length) ) { return false; }
-    if( !this.validate_begin_end_date( b[0].trim() ) ) { return false; }
+    if( !validate_begin_end_date( b[0].trim() ) ) { return false; }
     if( !jtregex.valid_real_number.test( b[1].trim() ) ) { return false; }
   }
   return true;
