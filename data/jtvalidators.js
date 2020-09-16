5e72da8142019a2db716c5af299fe8de6036c4b1
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-09-16 10:38:08 +0200

    implemented validate_begin_end_date

--- data/jtvalidators.js
+++ data/jtvalidators.js
@@ -22,18 +22,12 @@ validate_meter_data: function( s, with_factor ) {
   return true;
 },
 
-validate_begin_date: function( s ) {
+validate_begin_end_date: function( s ) {
   if( !jtregex.valid_date.test( s ) ) { return false; }
   //const mon = 1 * str.slice(5, 7);
   //const mid_month_begin_date = [16,15,16,16,16,...]
   const day = 1 * s.slice(8, 10);
   return 1 == day || 15 == day || 16 == day;
-},
-
-validate_end_date: function( s ) {
-  if( !jtregex.valid_date.test( s ) ) { return false; }
-  const day = 1 * str.slice(8, 10);
-  return 1 == day || 14 == day || 15 == day;
 },
 
 validate_dict_date_amount_string: function( s ) {
@@ -42,7 +36,7 @@ validate_dict_date_amount_string: function( s ) {
   for( let i = 0; i < n; ++i ) {
     var b = a[i].split( ':' );
     if( !(2 === b.length) ) { return false; }
-    if( !this.validate_begin_date( b[0].trim() ) ) { return false; }
+    if( !this.validate_begin_end_date( b[0].trim() ) ) { return false; }
     if( !jtregex.valid_real_number.test( b[1].trim() ) ) { return false; }
   }
   return true;
--- data/loaddata.test.js
+++ data/loaddata.test.js
@@ -231,10 +231,11 @@ test('contract has valid apartment, occupants, begin date, and later end', () =>
     //  ? true
     //  : (value.begin.getTime() < value.end.getTime());
     //expect(end_is_null_or_later_than_begin).toBeTruthy();
-    expect(value.begin).toBeInstanceOf(string);
+    expect( (typeof value.begin === 'string') || (value.begin instanceof String) ).toBeTruthy();
     expect(jtvalidators.validate_begin_date(value.begin)).toBeTruthy();
     if( value.end ) {
       expect( value.begin.localeCompare( value.end ) < 0 ).toBeTruthy();
+      console.log( value.end );
       expect( jtvalidators.validate_end_date(value.end) ).toBeTruthy();
     }
   }
