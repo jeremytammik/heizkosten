b750229051700fda6aae38f828e1088351eabb4f
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-12-10 10:17:39 +0100

    removed const allocation test year 2018 contract no longer exists

--- app/calc/energycostalloc.test.js
+++ app/calc/energycostalloc.test.js
@@ -2,8 +2,11 @@ const loaddata = require('../../data/loaddata');
 const Energycostalloc = require('./energycostalloc');
 
 test( 'test energycostalloc', () => {
-  var ids = ["001-12-06-2018"]; // Object.keys( loaddata.contracts );
-  ids.forEach( (id) => {
-    var a = new Energycostalloc( id, 2018 );
-  });
+  const mandy_schmidt_exists = false;
+  if(mandy_schmidt_exists) {
+    var ids = ["001-12-06-2018"]; // Object.keys( loaddata.contracts );
+    ids.forEach( (id) => {
+      var a = new Energycostalloc( id, 2018 );
+    });
+  }
 });
